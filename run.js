c364da8617e792fda3cb3c82098e50d688c97d43
Jake Lee <kc80.lee@samsung.com>
2018-02-12 10:44:00 +0900

    add_maker

--- run.js
+++ run.js
@@ -1,1 +1,9 @@
-console.log('Hello World');
+var add_maker = function(a) {
+    return function(b) { // closure. a is not in this function context.
+        return a + b;
+    }
+}
+//var add_maker = a => b => a + b;
+var add10 = add_maker(10);
+console.log(add10(20));
+console.log(add_maker(10)(20));
