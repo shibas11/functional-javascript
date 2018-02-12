4265e0b4ca9572f2358a40ee8d97b83e4ed60e58
Jake Lee <kc80.lee@samsung.com>
2018-02-12 10:51:00 +0900

    first class function

--- run.js
+++ run.js
@@ -7,3 +7,14 @@ var add_maker = function(a) {
 var add10 = add_maker(10);
 console.log(add10(20));
 console.log(add_maker(10)(20));
+
+function f4(f1, f2, f3) { // first class function
+    return f3(f1() + f2());
+}
+console.log(
+    f4(
+        () => 1,
+        () => 2,
+        a => a * a
+    )
+);
