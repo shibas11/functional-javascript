5ed05bd5a5487dbf636b03464d562efcd4438b20
Jake Lee <shibas11@gmail.com>
2018-04-21 19:17:23 +0900

    reformat

--- myLib.js
+++ myLib.js
@@ -1,27 +1,27 @@
 // 커링(_curry)
 const _curry = fn =>
-    (a, b) => b // 2개 인자 받을 경우에도 바로 돌도록 개선
-        ? fn(a, b)
-        : b => fn(a, b);
+    (a, b) => b ? // 2개 인자 받을 경우에도 바로 돌도록 개선
+        fn(a, b) :
+        b => fn(a, b);
 
 const _curry1 = fn =>
-    (a, b) => arguments.length == 2 // curry1은 제대로 동작 안 함!
-        ? fn(a, b)                  // arrow function을 쓰면 arguments가
-        : b => fn(a, b);            // 정상적으로 동작하지 않음.
+    (a, b) => arguments.length == 2 ?   // curry1은 제대로 동작 안 함!
+        fn(a, b) :                      // arrow function을 쓰면 arguments가
+        b => fn(a, b);                  // 정상적으로 동작하지 않음.
 
 const _curry2 = function (fn) {      // 이렇게 구현하면 OK
     return function (a, b) {
-        return arguments.length == 2
-            ? fn(a, b)
-            : b => fn(a, b);    // 여기는 arrow function을 썼지만
+        return arguments.length == 2 ?
+            fn(a, b) :
+            b => fn(a, b);      // 여기는 arrow function을 썼지만
     };                          // arguments를 사용하지 않는다
 };
 
 // 커링(_curryr), 오른쪽부터 평가하는 커링함수
 const _curryr = fn =>
-    (a, b) => b
-        ? fn(a, b)
-        : b => fn(b, a); // 여기만 순서가 바뀜
+    (a, b) => b ?
+        fn(a, b) :
+        b => fn(b, a); // 여기만 순서가 바뀜
 
 // _get
 const _get = _curryr((obj, key) => obj == null ? undefined : obj[key]);
@@ -78,18 +78,14 @@ const _reduce = (list, iter, acc) => {
 // pipe 를 더 추상화하면 reduce 가 됨.
 // pipe 는 함수들을 받도록 특화된 버전
 const _pipe = (...fnList) => {
-    const list = fnList[0].constructor === Array.prototype.constructor
-        ? fnList[0]
-        : fnList;
+    const list = fnList[0].constructor === Array.prototype.constructor ? fnList[0] : fnList;
 
     return acc => _reduce(list, (acc, fn) => fn(acc), acc);
 };
 
 // go 함수는 즉시 실행되는 pipe 함수
 const _go = (acc, ...fnList) => {
-    const list = fnList[0].constructor === Array.prototype.constructor
-        ? fnList[0]
-        : fnList;
+    const list = fnList[0].constructor === Array.prototype.constructor ? fnList[0] : fnList;
 
     return _pipe.apply(null, fnList)(acc);
 };
